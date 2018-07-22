830b48b3b7751597531398d8c1d942e99dba7412
Jack Baron <jackmbaron@gmail.com>
2018-07-22 21:49:06 +0100

    Add project files.

---
+++ ConfigManager/Misc/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigManager.Misc
+{
+    class Logger
+    {
+        public static void Log(string s)
+        {
+            Console.WriteLine("[Config Manager]: " + s);
+        }
+    }
+}
---
+++ ConfigManager/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ConfigManager.Misc;
+using IllusionPlugin;
+
+namespace ConfigManager
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "ConfigManager";
+        public string Version => "0.1.0";
+
+        public void OnApplicationStart()
+        {
+            Logger.Log("Loaded!");
+        }
+
+        public void OnApplicationQuit()
+        {
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+        }
+
+        public void OnLevelWasInitialized(int level)
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+    }
+}
