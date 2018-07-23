9ced336535252f54b3e52d957b98a31c1e0103a9
Jack Baron <jackmbaron@gmail.com>
2018-07-23 02:03:48 +0100

    Add Setter Class

---
+++ ConfigManager/Interface/Setter.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using Nett;
+
+namespace ConfigManager.Interface
+{
+    class Setter
+    {
+        private TomlTable _settings;
+
+        public Setter(TomlTable settings) { _settings = settings; }
+
+        public void AttatchComments(TomlObject field, string comment)
+        {
+            if (comment != "")
+            {
+                field.ClearComments();
+                field.AddComment(comment);
+            }
+        }
+
+        public TomlObject SetValueInternal(string key, bool value, string comment)
+        {
+            TomlObject field = _settings.ContainsKey(key) ?
+                _settings.Update(key, value) :
+                field = _settings.Add(key, value);
+
+            AttatchComments(field, comment);
+            return field;
+        }
+
+        public TomlObject SetValueInternal(string key, string value, string comment)
+        {
+            TomlObject field = _settings.ContainsKey(key) ?
+                _settings.Update(key, value) :
+                field = _settings.Add(key, value);
+
+            AttatchComments(field, comment);
+            return field;
+        }
+
+        public TomlObject SetValueInternal(string key, int value, string comment)
+        {
+            TomlObject field = _settings.ContainsKey(key) ?
+                _settings.Update(key, value) :
+                field = _settings.Add(key, value);
+
+            AttatchComments(field, comment);
+            return field;
+        }
+
+        public TomlObject SetValueInternal(string key, float value, string comment)
+        {
+            TomlObject field = _settings.ContainsKey(key) ?
+                _settings.Update(key, value) :
+                field = _settings.Add(key, value);
+
+            AttatchComments(field, comment);
+            return field;
+        }
+
+        public TomlObject SetValueInternal(string key, double value, string comment)
+        {
+            TomlObject field = _settings.ContainsKey(key) ?
+                _settings.Update(key, value) :
+                field = _settings.Add(key, value);
+
+            AttatchComments(field, comment);
+            return field;
+        }
+
+        public TomlObject SetValueInternal(string key, long value, string comment)
+        {
+            TomlObject field = _settings.ContainsKey(key) ?
+                _settings.Update(key, value) :
+                field = _settings.Add(key, value);
+
+            AttatchComments(field, comment);
+            return field;
+        }
+
+        public TomlObject SetValueInternal(string key, IEnumerable<bool> value, string comment)
+        {
+            TomlObject field = _settings.ContainsKey(key) ?
+                _settings.Update(key, value) :
+                field = _settings.Add(key, value);
+
+            AttatchComments(field, comment);
+            return field;
+        }
+
+        public TomlObject SetValueInternal(string key, IEnumerable<string> value, string comment)
+        {
+            TomlObject field = _settings.ContainsKey(key) ?
+                _settings.Update(key, value) :
+                field = _settings.Add(key, value);
+
+            AttatchComments(field, comment);
+            return field;
+        }
+
+        public TomlObject SetValueInternal(string key, IEnumerable<int> value, string comment)
+        {
+            TomlObject field = _settings.ContainsKey(key) ?
+                _settings.Update(key, value) :
+                field = _settings.Add(key, value);
+
+            AttatchComments(field, comment);
+            return field;
+        }
+
+        public TomlObject SetValueInternal(string key, IEnumerable<float> value, string comment)
+        {
+            TomlObject field = _settings.ContainsKey(key) ?
+                _settings.Update(key, value) :
+                field = _settings.Add(key, value);
+
+            AttatchComments(field, comment);
+            return field;
+        }
+
+        public TomlObject SetValueInternal(string key, IEnumerable<double> value, string comment)
+        {
+            TomlObject field = _settings.ContainsKey(key) ?
+                _settings.Update(key, value) :
+                field = _settings.Add(key, value);
+
+            AttatchComments(field, comment);
+            return field;
+        }
+
+        public TomlObject SetValueInternal(string key, IEnumerable<long> value, string comment)
+        {
+            TomlObject field = _settings.ContainsKey(key) ?
+                _settings.Update(key, value) :
+                field = _settings.Add(key, value);
+
+            AttatchComments(field, comment);
+            return field;
+        }
+    }
+}
--- ConfigManager/Manager.cs
+++ ConfigManager/Manager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using ConfigManager.Interface;
 using Nett;
 
 namespace ConfigManager
@@ -16,6 +17,9 @@ namespace ConfigManager
         // Data Values
         TomlTable _toml;
         TomlTable _settings;
+
+        // Interfaces
+        private Setter _setter;
 
         public static void EnsureConfigFile()
         {
@@ -34,6 +38,7 @@ namespace ConfigManager
 
         public Manager(string _modName)
         {
+            // Set Mod Name
             ModName = _modName;
 
             // Read in current config and add a new table for self.
@@ -51,12 +56,33 @@ namespace ConfigManager
                 _settings = _toml.Get<TomlTable>(ModName);
             }
 
+            // Define Interfaces
+            _setter = new Setter(_settings);
+
             Flush();
         }
 
-        public void Flush()
-        {
-            Toml.WriteFile(_toml, FilePath);
-        }
+        public void Flush() { Toml.WriteFile(_toml, FilePath);  }
+
+        // So...
+        // To anyone looking at the next lines of code: I'm sorry.
+        // The TOML library I'm using DOESN'T HAVE GENERICS
+        // Well it does. BUT ONLY PRIVATE METHODS.
+        // I'm also probably using it wrong.
+        // C# is not my strongest language.
+
+        // Handle all the fucking data types.
+        public void Set(string key, bool value, string comment = "") { _setter.SetValueInternal(key, value, comment); Flush(); }
+        public void Set(string key, string value, string comment = "") { _setter.SetValueInternal(key, value, comment); Flush(); }
+        public void Set(string key, int value, string comment = "") { _setter.SetValueInternal(key, value, comment); Flush(); }
+        public void Set(string key, float value, string comment = "") { _setter.SetValueInternal(key, value, comment); Flush(); }
+        public void Set(string key, double value, string comment = "") { _setter.SetValueInternal(key, value, comment); Flush(); }
+        public void Set(string key, long value, string comment = "") { _setter.SetValueInternal(key, value, comment); Flush(); }
+        public void Set(string key, IEnumerable<bool> value, string comment = "") { _setter.SetValueInternal(key, value, comment); Flush(); }
+        public void Set(string key, IEnumerable<string> value, string comment = "") { _setter.SetValueInternal(key, value, comment); Flush(); }
+        public void Set(string key, IEnumerable<int> value, string comment = "") { _setter.SetValueInternal(key, value, comment); Flush(); }
+        public void Set(string key, IEnumerable<float> value, string comment = "") { _setter.SetValueInternal(key, value, comment); Flush(); }
+        public void Set(string key, IEnumerable<double> value, string comment = "") { _setter.SetValueInternal(key, value, comment); Flush(); }
+        public void Set(string key, IEnumerable<long> value, string comment = "") { _setter.SetValueInternal(key, value, comment); Flush(); }
     }
 }
